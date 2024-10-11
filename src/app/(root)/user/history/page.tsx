cf31c500b694a83daa509b05a5b13bf5e5873d9d
Phuc Thien <phucthien@gmail.com>
2024-10-11 20:18:09 +0700

    Add User Profile Pages And Footer

---
+++ src/app/(root)/user/history/page.tsx
@@ -0,0 +1,13 @@
+import History from "@/components/pages/User/History/History"
+
+export default function page() {
+    return (
+        <>
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+            </head>
+
+            <History />
+        </>
+    )
+}
---
+++ src/app/(root)/user/password/page.tsx
@@ -0,0 +1,13 @@
+import Password from "@/components/pages/User/Password/Password";
+
+export default function page() {
+    return (
+        <>
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+            </head>
+
+            <Password />
+        </>
+    )
+}
